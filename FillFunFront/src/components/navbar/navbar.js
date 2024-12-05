03534bf66179fcf4bd03ca29d7b2c9cab250bdc6
saja-massimi <massimisaja@gmail.com>
2024-12-05 15:22:54 +0300

    added all routes

--- FillFunFront/src/components/navbar/navbar.js
+++ FillFunFront/src/components/navbar/navbar.js
@@ -1,22 +1,28 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "../../assets/logo_white.png";
-import '../../assets/css/main.css'
+
 function Navbar() {
     const [isDropdownOpen, setDropdownOpen] = useState(false);
     const [isDeepDropdownOpen, setDeepDropdownOpen] = useState(false);
     const [isScrolled, setScrolled] = useState(false);
     const [isMobileNavActive, setMobileNavActive] = useState(false);
-    
+
     useEffect(() => {
         const handleScroll = () => {
-            setScrolled(window.scrollY > 100);
+
+            if (window.scrollY > 100) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+
         };
 
         window.addEventListener("scroll", handleScroll);
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
-    }, []);
+    }, [isScrolled]);
 
     const toggleMobileNav = () => {
         setMobileNavActive((prevState) => !prevState);
@@ -35,21 +41,26 @@ function Navbar() {
     return (
         <>
 
+            <header
+                id="header" style={isScrolled ? {
+                    backgroundColor: '#10058c'
+                } : {
 
-            <header id="header" className="header d-flex align-items-center fixed-top">
+                    backgroundColor: 'transparent'
+                }}
+
+
+                className={`header  d-flex align-items-center fixed-top ${isScrolled ? 'scrolled' : ''}`}
+            >
                 <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
-
                     <a href="index.html" className="logo d-flex align-items-center">
                         <img src={Logo} alt="Logo" />
                     </a>
 
-
-                    <nav id="navmenu" className="navmenu">
+                    <nav id="navmenu" className={`navmenu ${isMobileNavActive ? 'mobile-nav-active' : ''}`}>
                         <ul>
                             <li>
-                                <a href="#hero" className="active">
-                                    Home
-                                </a>
+                                <a href="#hero" className="active">Home</a>
                             </li>
                             <li>
                                 <a href="#about">About</a>
@@ -69,59 +80,37 @@ function Navbar() {
                             {/* Dropdown */}
                             <li className={`dropdown ${isDropdownOpen ? "show" : ""}`}>
                                 <a href="#" onClick={toggleDropdown}>
-                                    <span>Dropdown</span>{" "}
+                                    <span>Dropdown</span>
                                     <i className="bi bi-chevron-down toggle-dropdown"></i>
                                 </a>
                                 <ul className={isDropdownOpen ? "show" : ""}>
-                                    <li>
-                                        <a href="#">Dropdown 1</a>
-                                    </li>
+                                    <li><a href="#">Dropdown 1</a></li>
                                     {/* Deep Dropdown */}
                                     <li className={`dropdown ${isDeepDropdownOpen ? "show" : ""}`}>
                                         <a href="#" onClick={toggleDeepDropdown}>
-                                            <span>Deep Dropdown</span>{" "}
+                                            <span>Deep Dropdown</span>
                                             <i className="bi bi-chevron-down toggle-dropdown"></i>
                                         </a>
                                         <ul className={isDeepDropdownOpen ? "show" : ""}>
-                                            <li>
-                                                <a href="#">Deep Dropdown 1</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Deep Dropdown 2</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Deep Dropdown 3</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Deep Dropdown 4</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Deep Dropdown 5</a>
-                                            </li>
+                                            <li><a href="#">Deep Dropdown 1</a></li>
+                                            <li><a href="#">Deep Dropdown 2</a></li>
+                                            <li><a href="#">Deep Dropdown 3</a></li>
                                         </ul>
                                     </li>
-                                    <li>
-                                        <a href="#">Dropdown 2</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Dropdown 3</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Dropdown 4</a>
-                                    </li>
+                                    <li><a href="#">Dropdown 2</a></li>
                                 </ul>
                             </li>
-                            <li>
-                                <a href="#contact">Contact</a>
-                            </li>
+                            <li><a href="#contact">Contact</a></li>
                         </ul>
-                        <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
+                        {/* Mobile Navigation Toggle */}
+                        <button className="mobile-nav-toggle d-xl-none" onClick={toggleMobileNav}>
+                            <i className={`bi ${isMobileNavActive ? 'bi-x' : 'bi-list'}`}></i>
+                        </button>
                     </nav>
                 </div>
             </header>
-
         </>
-    )
+    );
 }
 
 export default Navbar;
