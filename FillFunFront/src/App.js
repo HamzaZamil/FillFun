be324f6d73facd7f704a22ccf65adaddd884269c
HamzaZamil <hamzaalzamil@gmail.com>
2024-12-07 00:08:02 +0300

    pulling

--- FillFunFront/src/App.js
+++ FillFunFront/src/App.js
@@ -8,7 +8,7 @@ import Quiz from './components/boards/quiz.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/vendor/swiper/swiper-bundle.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './assets/vendor/php-email-form/validate.js';
+// import './assets/vendor/php-email-form/validate.js';
 
 import './assets/vendor/purecounter/purecounter_vanilla.js';
 import './assets/vendor/swiper/swiper-bundle.min.js';
@@ -17,6 +17,8 @@ import 'aos/dist/aos.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/auth/login.jsx';
+import Register from './components/auth/register.jsx';
 
 
 const ScrollToTopButton = ({ isVisible, onClick }) => {
@@ -64,6 +66,8 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/boards" element={<Boards />} />
           <Route path="/quiz" element={<Quiz />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
 
         <Footer />
